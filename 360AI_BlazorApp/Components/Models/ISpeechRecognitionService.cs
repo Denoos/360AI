71e246f697bc346ab69e55e854e947991ec110a3
Denoos <danil2006timofeev@gmail.com>
2025-11-27 14:58:01 +1000

    TestCommit

---
+++ 360AI_BlazorApp/Components/Models/ISpeechRecognitionService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.ComponentModel;
+
+namespace _360AI_BlazorApp.Components.Models
+{
+    public interface ISpeechRecognitionService : IAsyncDisposable
+    {
+        Task CancelSpeechRecognitionAsync(bool isAborted);
+
+        Task<IDisposable> RecognizeSpeechAsync(
+            string language,
+            Func<string, Task> onRecognized,
+            Func<SpeechRecognitionErrorEvent, Task>? onError = null,
+            Func<Task>? onStarted = null,
+            Func<Task>? onEnded = null);
+    }
+}
--- 360AI_BlazorApp/Program.cs
+++ 360AI_BlazorApp/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddSpeechRecognitionServices();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
