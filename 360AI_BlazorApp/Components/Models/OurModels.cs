b330866252860d1c8b3e39da45abe11da9d8362c
Denoos <danil2006timofeev@gmail.com>
2025-11-28 11:41:27 +1000

    d

--- 360AI_BlazorApp/Components/Models/OurModels.cs
+++ 360AI_BlazorApp/Components/Models/OurModels.cs
@@ -3,11 +3,11 @@
 namespace _360AI_BlazorApp.Components.Models
 {
     public class OurModels
-    {
+{
         public string Id { get; set; }
         public string Content { get; set; }
         public string MediaType { get; set; }
         public double Relevance { get; set; }
         public string Source { get; set; }
-    }
+}
 }
